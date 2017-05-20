f7c5f9078fffd021d2407b37f4eaaadfac10ad92
ss <you@example.com>
2017-05-20 19:57:13 +0100

    s

--- moneyCalculator.java
+++ moneyCalculator.java
@@ -11,7 +11,7 @@ import java.util.Scanner;
 public class moneyCalculator {
 	public static void main(String[] args) {
 		Scanner myScanner = new Scanner(System.in);
-		System.out.println("Enter number : ");
+		System.out.println("Enter  number : ");
 		int number = myScanner.nextInt();
 		total(number);
 	}
